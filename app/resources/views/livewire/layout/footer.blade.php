30c0863d937b290206b3ec6c392795677864a5bf
lucifilho <lucielfilho@hotmail.com>
2024-10-13 10:04:02 -0300

    Adiciona Header e Footer

--- app/resources/views/livewire/layout/footer.blade.php
+++ app/resources/views/livewire/layout/footer.blade.php
@@ -1,3 +1,23 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+
+    <footer>
+
+        <div class="rodape">
+
+            <div class="politica-termos">
+
+                <a href="/politica-de-privacidade">Politica de Privacidade</a>
+
+                <a href="/termos-e-condicoes">Termos e Condições</a>
+
+            </div>
+
+            <div class="copy">
+                &copy; {{date('Y')}} Carefy - Todos os direitos reservados.
+            </div>
+
+        </div>
+
+    </footer>
+
 </div>
--- app/resources/views/livewire/layout/header.blade.php
+++ app/resources/views/livewire/layout/header.blade.php
@@ -1,3 +1,81 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+
+    <header>
+
+        <nav class="navbar navbar-expand-lg">
+
+            <div class="container-fluid">
+
+                <a class="navbar-brand" href="/">
+                    <img class="logo" src="/imagens/logo.svg">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+
+
+                            <li class="nav-item dropdown">
+
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Pacientes
+                                </a>
+
+                                <ul class="dropdown-menu">
+
+                                    <li><a class="dropdown-item" href="/lista-pacientes">Meus Pacientes</a></li>
+                                    <li><a class="dropdown-item" href="/novo-paciente">Novo Paciente</a></li>
+
+                                </ul>
+
+                            </li>
+
+                            <li class="nav-item dropdown">
+
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                </a>
+
+                                <ul class="dropdown-menu">
+
+                                    <li><a class="dropdown-item" href="/user/profile">Minha Conta</a></li>
+
+                                    <li>
+                                        <form action="/logout" method="POST">
+                                            @csrf
+                                            <a class="dropdown-item"
+                                               href="/logout"
+                                               onclick="event.preventDefault();
+                            this.closest('form').submit();"
+                                            >Sair</a>
+                                        </form>
+                                    </li>
+                                </ul>
+
+                            </li>
+
+
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/suporte">Suporte</a>
+                            </li>
+
+
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </nav>
+
+    </header>
+
 </div>
