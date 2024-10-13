1ef2d056b79e6641e6839c33c7ba901713723784
lucifilho <lucielfilho@hotmail.com>
2024-10-13 10:32:55 -0300

    Adiciona Acesso Rápido

--- app/resources/views/pages/home.blade.php
+++ app/resources/views/pages/home.blade.php
@@ -1,9 +1,35 @@
 @extends('layouts.main')
 @section('content')
 
-    <div class="home">
+    <div class="home bloco-pagina">
 
-        <livewire:pacientes.lista-pacientes/>
+        <div class="acesso-rapido bloco-conteudo">
+
+            <a class="item" href="/lista-pacientes">
+
+                <i class="fa fa-users"></i>
+
+                Lista de Pacientes
+
+            </a>
+
+            <a class="item" href="/novo-paciente">
+
+                <i class="fa fa-user-plus"></i>
+
+                Novo Pacientes
+
+            </a>
+
+            <a class="item" href="/suporte">
+
+                <i class="fa fa-question"></i>
+
+                Suporte
+
+            </a>
+
+        </div>
 
     </div>
 
