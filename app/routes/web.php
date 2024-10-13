1541951fb9f2bc7a2a8c55228fc71537d01a1a77
lucifilho <lucielfilho@hotmail.com>
2024-10-13 10:32:05 -0300

    Adiciona Rotas: Home/Pacientes/Suporte

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeralController;
+use App\Http\Controllers\PacienteController;
+
 
 Route::get('/', [GeralController::class,'home']);
+Route::get('/lista-pacientes', [PacienteController::class,'index']);
+Route::get('/novo-paciente', [PacienteController::class,'novoPaciente']);
+Route::get('/suporte', [GeralController::class,'suporte']);
 
