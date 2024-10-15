1dc6c735e5e137165474157500d93aae4b662d3d
lucifilho <lucielfilho@hotmail.com>
2024-10-14 22:09:02 -0300

    readequa rotas

--- app/routes/web.php
+++ app/routes/web.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeralController;
 use App\Http\Controllers\PacienteController;
 
+Route::get('/novo-paciente', [PacienteController::class,'novoPaciente']);
+Route::post('/cadastrar', [PacienteController::class, 'cadastrarPaciente']);
+Route::post('/importar-planilha', [PacienteController::class, 'importarPlanilha']);
+Route::post('/salvar-planilha', [PacienteController::class, 'salvarDadosPlanilha']);
+Route::get('/pacientes', [PacienteController::class,'index']);
 
 Route::get('/', [GeralController::class,'home']);
-Route::get('/lista-pacientes', [PacienteController::class,'index']);
-Route::get('/novo-paciente', [PacienteController::class,'novoPaciente']);
 Route::get('/suporte', [GeralController::class,'suporte']);
 
