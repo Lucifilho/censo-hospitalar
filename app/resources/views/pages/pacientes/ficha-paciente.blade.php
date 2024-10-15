ba9ddc83c2bbf16e59b01d4116d08b8a669aa6f0
lucifilho <lucielfilho@hotmail.com>
2024-10-15 00:07:57 -0300

    Adiciona as internações

--- app/resources/views/pages/pacientes/ficha-paciente.blade.php
+++ app/resources/views/pages/pacientes/ficha-paciente.blade.php
@@ -1,16 +1,22 @@
-<div class="ficha-paciente">
+@extends('layouts.main')
+@section('content')
 
-    <div class="formulario-paciente">
-        <h2>Ficha do Paciente</h2>
+    <div class="ficha-paciente">
 
-        <form wire:submit.prevent="submit">
+        <div class="bloco-paciente">
 
-            <div class="linha">
+            <h2>Ficha do Paciente</h2>
+
+            <form action="/atualizar-paciente" method="post" class="formulario">
+                @csrf
+                @method('PUT')
+
+                <input type="hidden" value="{{$paciente -> id}}" name="id">
 
                 <div class="grupo">
 
                     <label for="nome">Nome</label>
-                    <input disabled type="text" id="nome" wire:model="nome">
+                    <input class="input" type="text" id="nome" value="{{$paciente -> nome}}" name="nome">
                     @error('nome') <span class="error">{{ $message }}</span> @enderror
 
                 </div>
@@ -18,49 +24,75 @@
                 <div class="grupo">
 
                     <label for="nascimento">Data de Nascimento</label>
-                    <input disabled type="date" id="nome" wire:model="nascimento">
+                    <input class="input" type="date" id="nome" value="{{$paciente -> nascimento}}" name="nascimento">
                     @error('nascimento') <span class="error">{{ $message }}</span> @enderror
 
                 </div>
 
-            </div>
+                <div class="linha">
 
-            <div class="linha">
+                    <div class="grupo">
 
-                <div class="grupo">
+                        <label for="codigo">Código</label>
+                        <input class="input" disabled type="text" id="codigo" value="{{$paciente -> codigo}}" name="codigo">
+                        @error('codigo') <span class="error">{{ $message }}</span> @enderror
 
-                    <label for="guia">Guia</label>
-                    <input disabled type="text" id="guia" wire:model="guia">
-                    @error('guia') <span class="error">{{ $message }}</span> @enderror
+                    </div>
 
-                </div>
+                    <div class="grupo">
 
-            </div>
+                        <label for="guia">Guia</label>
+                        <input class="input" disabled type="text" id="guia" value="{{$paciente -> guia}}" name="guia">
+                        @error('guia') <span class="error">{{ $message }}</span> @enderror
 
-            <div class="linha">
-
-                <div class="grupo">
-
-                    <label for="entrada">Entrada</label>
-                    <input disabled type="date" id="entrada" wire:model="entrada">
-                    @error('entrada') <span class="error">{{ $message }}</span> @enderror
+                    </div>
 
                 </div>
 
-                <div class="grupo">
+                <div class="linha">
 
-                    <label for="saida">Saída</label>
-                    <input disabled type="date" id="saida" wire:model="saida">
-                    @error('saida') <span class="error">{{ $message }}</span> @enderror
+                    <div class="grupo">
+
+                        <label for="entrada">Entrada</label>
+                        <input class="input" type="date" id="entrada" value="{{$paciente -> entrada}}" name="entrada">
+                        @error('entrada') <span class="error">{{ $message }}</span> @enderror
+
+                    </div>
+
+                    <div class="grupo">
+
+                        <label for="saida">Saída</label>
+                        <input class="input" type="date" id="saida" value="{{$paciente -> saida}}" name="saida">
+                        @error('saida') <span class="error">{{ $message }}</span> @enderror
+
+                    </div>
 
                 </div>
 
+                <button type="submit">Atualizar</button>
+
+            </form>
+
+            <div class="qnt-internacoes">
+                <h3>Internações</h3>
+
+                    <ul>
+                        @foreach($internacoes as $internacao)
+                            <li>
+
+                                <strong>Guia:</strong> {{ $internacao->guia }}<br>
+                                <strong>Data de Entrada:</strong> {{ $internacao->entrada }}<br>
+                                <strong>Data de Saída:</strong> {{ $internacao->saida ?? 'Ainda internado' }}<br>
+
+                            </li>
+                        @endforeach
+
+                    </ul>
             </div>
 
-            <button type="submit">Atualizar</button>
 
-        </form>
+        </div>
 
     </div>
 
-</div>
+@endsection
