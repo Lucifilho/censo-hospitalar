b1f1d2da5840ece19fcd950bc17270ecfe94fc64
lucifilho <lucielfilho@hotmail.com>
2024-10-15 00:06:11 -0300

    Modifica o menu

--- app/resources/views/livewire/layout/header.blade.php
+++ app/resources/views/livewire/layout/header.blade.php
@@ -22,55 +22,13 @@
                             <a class="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
 
-                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/pacientes">Pacientes</a>
+                        </li>
 
-                            <li class="nav-item dropdown">
-
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Pacientes
-                                </a>
-
-                                <ul class="dropdown-menu">
-
-                                    <li><a class="dropdown-item" href="/lista-pacientes">Meus Pacientes</a></li>
-                                    <li><a class="dropdown-item" href="/novo-paciente">Novo Paciente</a></li>
-
-                                </ul>
-
-                            </li>
-
-                            <li class="nav-item dropdown">
-
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Olá, {{$user -> name }}
-                                </a>
-
-                                <ul class="dropdown-menu">
-
-                                    <li><a class="dropdown-item" href="/user/profile">Minha Conta</a></li>
-
-                                    <li>
-                                        <form action="/logout" method="POST">
-                                            @csrf
-                                            <a class="dropdown-item"
-                                               href="/logout"
-                                               onclick="event.preventDefault();
-                            this.closest('form').submit();"
-                                            >Sair</a>
-                                        </form>
-                                    </li>
-                                </ul>
-
-                            </li>
-
-                        @elseguest
-
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/suporte">Suporte</a>
-                            </li>
-
-                        @endauth
-
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/suporte">Suporte</a>
+                        </li>
 
                     </ul>
 
