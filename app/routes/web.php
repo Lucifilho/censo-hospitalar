b955d4947836496f01b78a7823f70c378f7bb945
lucifilho <lucielfilho@hotmail.com>
2024-10-15 00:07:04 -0300

    Adiciona a rota para Edição do registro

--- app/routes/web.php
+++ app/routes/web.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\PacienteController;
 Route::get('/novo-paciente', [PacienteController::class,'novoPaciente']);
 Route::post('/importar-planilha', [PacienteController::class, 'importarPlanilha']);
 Route::post('/salvar-planilha', [PacienteController::class, 'salvarDadosPlanilha']);
-Route::get('/pacientes', [PacienteController::class,'index']);
+Route::put('/atualizar-paciente', [PacienteController::class, 'update']);
 
+Route::get('/pacientes', [PacienteController::class,'index']);
+Route::get('/paciente-{codigo}', [PacienteController::class,'show']);
 Route::get('/', [GeralController::class,'home']);
 Route::get('/suporte', [GeralController::class,'suporte']);
 
